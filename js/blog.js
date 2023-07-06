23bceb46788dcc6ef527caf706ddda3b6241a48a
laura <contact0faztweb.com>
2023-07-06 13:19:24 +0200

    mod navbar

--- js/blog.js
+++ js/blog.js
@@ -3,6 +3,12 @@ document.querySelector('.menu-icon').addEventListener('click', function () {
     document.querySelector('.menu').classList.toggle('show');
 });
 
+$(document).ready(function() {
+    $('.menu-icon').click(function() {
+      $('.menu-popup').toggleClass('show');
+    });
+  });
+
 $(document).ready(function () {
     $('.slider').bxSlider({
         minSlides: 1, // Mínimo de slides a mostrar
@@ -12,5 +18,7 @@ $(document).ready(function () {
         pause: 4000, // Tiempo de pausa entre cada slide (en milisegundos)
         speed: 500, // Velocidad de transición entre slides (en milisegundos)
         controls: false, // Sin flechas para pasar las imagenes
+        pager: false,
+        // pagerType: false,
     });
 });
